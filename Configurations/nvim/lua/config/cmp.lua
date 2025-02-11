482313bf307a50e44a046df06115de4d3091d2fc
Attila Mészely <attila.meszely@dmgmori-heitec.hu>
2025-02-11 14:30:11 +0100

    update

--- Configurations/nvim/lua/config/cmp.lua
+++ Configurations/nvim/lua/config/cmp.lua
@@ -50,4 +50,15 @@ cmp.setup.cmdline(':', {
   })
 })
 
-
+local cmp = require('cmp')
+cmp.setup({
+  mapping = {
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    -- Add other completion sources as needed
+  },
+})
