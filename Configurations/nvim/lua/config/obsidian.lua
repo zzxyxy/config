91a31e29559b1a73256136ba88a608e1cc924cfe
zxyxy <zxyxy@protonmail.com>
2025-02-21 14:57:00 +0100

    update

---
+++ Configurations/nvim/lua/config/obsidian.lua
@@ -0,0 +1,13 @@
+require("obsidian").setup({
+   workspaces = {
+     {
+       name = "personal",
+       path = "~/vault/personal/",
+     },
+     {
+       name = "work",
+       path = "~/vault/work",
+     },
+   },
+      -- see below for full list of options 👇
+})
---
+++ Configurations/nvim/lua/config/terraform.lua
@@ -0,0 +1,5 @@
+-- Enable autoformatting for Terraform files
+vim.g.terraform_fmt_on_save = 1
+-- Automatically fold sections
+vim.g.terraform_align = 1
+	 
